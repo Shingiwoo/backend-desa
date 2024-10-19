040c50eb0d8e9e0745f2573cb9fb65a920e0d936
shingiwoo <shingiwoo.ind@gmail.com>
2024-10-19 20:49:00 +0700

    RESTful API CRUD Sliders & Aparaturs

--- routes/api.php
+++ routes/api.php
@@ -64,5 +64,13 @@ Route::prefix('admin')->group(function () {
         //Photos
         Route::apiResource('/photos', App\Http\Controllers\Api\Admin\PhotoController::class, ['except' => ['create', 'show', 'update']])
             ->middleware('permission:photos.index|photos.store|photos.delete');
+
+        //Sliders
+        Route::apiResource('/sliders', App\Http\Controllers\Api\Admin\SliderController::class, ['except' => ['create', 'show', 'update']])
+            ->middleware('permission:sliders.index|sliders.store|sliders.delete');
+
+        //Aparaturs
+        Route::apiResource('/aparaturs', App\Http\Controllers\Api\Admin\AparaturController::class)
+            ->middleware('permission:aparaturs.index|aparaturs.store|aparaturs.update|aparaturs.delete');
     });
 });
